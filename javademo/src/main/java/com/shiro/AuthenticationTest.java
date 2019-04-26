89ce27deec47c22b96539fc6ff482278703a368c
xiongyf <xiongyf1@gmail.com>
2019-04-26 22:44:44 +0800

    测试IniRealm

--- javademo/src/main/java/com/shiro/AuthenticationTest.java
+++ javademo/src/main/java/com/shiro/AuthenticationTest.java
@@ -4,6 +4,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,10 +16,9 @@ public class AuthenticationTest {
 
     @Before
     public void addUser() {
-        realm.addAccount("admin1", "123456","admin","staff");
+       // realm.addAccount("admin1", "123456","admin","staff");
     }
 
-    @Test
     public void authenticate() {
 
         // 构建securityManager环境
@@ -36,6 +36,28 @@ public class AuthenticationTest {
         subject.logout();
         System.out.println(subject.isAuthenticated());
 
+    }
+
+    @Test
+    public void iniRealmTest(){
+
+        IniRealm realm=new IniRealm("classpath:user.ini");
+
+        // 构建securityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(realm);
+
+        // 主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("Mark", "123456");
+        subject.login(token);
+        System.out.println(subject.isAuthenticated());
+        System.out.println(subject.getPrincipal());//用户名
+        subject.checkRole("admin");
+        subject.checkPermission("user:add");
+        subject.logout();
+        System.out.println(subject.isAuthenticated());
     }
 
 }
